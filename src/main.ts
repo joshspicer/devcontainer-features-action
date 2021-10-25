3227f96a1d25f9191658d4c8079e2da94a7af6d6
Josh Spicer <hello@joshspicer.com>
2021-10-25 21:17:49 +0000

    refactor to do less

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,20 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import {createRelease, getReleaseByTag, updateReleaseAssetsWithFeaturesDir} from './utils'
+import { tarFeaturesDirectory } from './utils'
 
 async function run(): Promise<void> {
   try {
     core.debug("Reading in secrets...")
-    // Hydrate GitHub API provider
     const token = core.getInput('token')
     const octokit = github.getOctokit(token)
 
     // Defaults to root directory, "."
     const featuresPath = core.getInput('path-to-features')
-    const tagName = core.getInput('tag-name')
 
-    core.debug(`Starting...`)
-    const release = await getReleaseByTag(octokit, tagName)
-    if (release) {
-      await updateReleaseAssetsWithFeaturesDir(
-        octokit,
-        release.data,
-        featuresPath
-      )
-    }
+    core.debug(`Starting...`);
+    core.debug("calling tarFeaturesDirectory()")
+    await tarFeaturesDirectory(featuresPath);
+    
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
--- src/utils.ts
+++ src/utils.ts
@@ -1,92 +1,13 @@
 import * as github from '@actions/github'
+import * as tar from 'tar'
+import * as fs from 'fs'
 import * as core from '@actions/core'
 import {Octokit} from '@octokit/core'
 import {Api} from '@octokit/plugin-rest-endpoint-methods/dist-types/types'
-import * as tar from 'tar'
-import * as fs from 'fs'
 
-// Creates a release at the current context's ref with the given tagName
-export async function createRelease(octokit: Octokit & Api, tagName: string) {
-  core.debug(`Starting to create release against ${github.context.ref}`);
-  const release = await octokit.rest.repos.createRelease({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    tag_name: tagName,
-    target_commitish: github.context.ref,
-    name: 'Features Release',
-    prerelease: true
-  });
-
-  if (release.status > 299) {
-    core.setFailed(`Could not create release. Status code: ${release.status}`)
-    return undefined
-  }
-
-  core.info(
-    `Created release (${release.status}) with tag '${tagName}' at ${release.data.upload_url}`
-  )
-  return release
-}
-
-export async function getReleaseByTag(octokit: Octokit & Api, tag: string) {
-  core.debug(`Fetching release for ${tag}`);
-  const release = await octokit.rest.repos.getReleaseByTag({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    tag,
-  });
-
-  if (release.status > 299) {
-    core.setFailed(`Could not fetch existing release. Status code: ${release.status}`)
-    return undefined
-  }
-
-  core.info(
-    `Fetched existing release (${release.status}) with tag '${tag}' at ${release.data.upload_url}`
-  )
-  return release
-}
-
-
-
-// Updates a provided release with the relevant features assets
-export async function updateReleaseAssetsWithFeaturesDir(
-  octokit: Octokit & Api,
-  release: {upload_url: string; id: number},
-  featuresPath: string
-) {
-  tar.create({file: 'features.tgz'}, [featuresPath]).then(_ => {
-    fs.readFile(
-      './features.tgz',
-      {
-        encoding: 'utf-8'
-      },
-      (err, data) => {
-        if (err) {
-          core.setFailed(err.message)
-        }
-
-        let uploaded = octokit.rest.repos
-          .uploadReleaseAsset({
-            owner: github.context.repo.owner,
-            repo: github.context.repo.repo,
-            release_id: release.id,
-            name: 'features.tgz',
-            data
-          })
-          .then(res => {
-            if (!res || res.status > 299) {
-              core.setFailed(
-                `Could not upload asset. Status Code: ${res.status}`
-              )
-            }
-            core.setOutput('uploaded-state', res.data.state)
-            core.setOutput(
-              'uploaded-browser-upload-url',
-              res.data.browser_download_url
-            )
-          })
-      }
-    )
-  })
+export async function tarFeaturesDirectory(path: string) {
+  return tar.create({file: 'features.tar.gz'}, [path])
+    .then(_ => {
+      core.info("Compressed features directory.");
+    });
 }
